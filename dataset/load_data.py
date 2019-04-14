b80f6840515dc31bba9e75c4920ce39f507f3678
guitarmonyz <guitarmonyz@gmail.com>
2019-04-14 11:27:39 -0400

    added data processing

--- dataset/load_data.py
+++ dataset/load_data.py
@@ -5,4 +5,45 @@ from pprint import pprint
 from time import time
 from tqdm import tqdm_notebook
 from scipy import sparse
-def get_lines(filename): return (line.split() for line in BZ2File(filename))
+import scipy.io
+def get_lines(filename): 
+	return (line.split() for line in BZ2File(filename))
+
+def get_redirect(targ, redirects):
+    seen = set()
+    while True:
+        transitive_targ = targ
+        targ = redirects.get(targ)
+        if targ is None or targ in seen: break
+        seen.add(targ)
+    return transitive_targ
+
+def get_redirects(redirects_filename):
+    redirects={}
+    lines = get_lines(redirects_filename)
+    return {src[SLICE]:get_redirect(targ[SLICE], redirects) 
+                for src,_,targ,_ in tqdm_notebook(lines, leave=False)}
+
+def add_item(lst, redirects, index_map, item):
+    k = item[SLICE]
+    lst.append(index_map.setdefault(redirects.get(k, k), len(index_map)))
+
+limit=119077682 #5000000
+redirects = get_redirects(redirects_filename)
+
+index_map = dict() # links->IDs
+lines = get_lines(page_links_filename)
+source, destination, data = [],[],[]
+for l, split in tqdm_notebook(enumerate(lines), total=limit):
+    if l >= limit: break
+    add_item(source, redirects, index_map, split[0])
+    add_item(destination, redirects, index_map, split[2])
+    data.append(1)
+
+X = sparse.coo_matrix((data, (destination,source)), shape=(n,n), dtype=np.float32)
+X = X.tocsr()
+
+del(data,destination, source)
+print(X.shape)
+print('saving data')
+scipy.io.savemat('./', mdict={'redirects':X})
